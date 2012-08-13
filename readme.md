44e5a5e344556ec6e7cdacb549e3529fc2504d5d
Bradley Rosenfeld <rosenfeld.bradley@gmail.com>
2012-08-12 22:25:52 -0500

    First Commit!
    Pushed everything up!

---
+++ readme.md
@@ -0,0 +1,9 @@
+#JS Clock, iPad Web App
+
+I created this clock web app on my iPad using Textastic as a fun little project (I was on vacation and bored :3).
+
+Right now it is a simple affair, it only displays the time and today's date.
+
+I included a manifest file so the app also works offline.
+
+Have fun!
---
+++ script.js
@@ -0,0 +1,42 @@
+//Created by Bradley Rosenfeld for iPad's everywhere!
+var timeDisplayElement = document.getElementById('timeDisplay');
+var timeElement = document.getElementById('date');
+timer();
+today();
+function timer() {
+	var time = new Date();
+	var second = time.getSeconds();
+	var minute = time.getMinutes();
+	var hour = time.getHours();
+		if (second < 10) {
+			second = "0" + second;
+		}
+		if (minute < 10) {
+			minute = "0" + minute;
+		}
+		if (hour > 12) {
+			hour = hour - 12;
+			period = "PM";
+		} else {
+			period = "AM";
+		}
+	var timeString = " <span class='period'>" + period + "</span>" + hour + ":" + minute + ":" + second;
+	timeDisplayElement.innerHTML = timeString;
+	window.setTimeout(today, 5000);
+	window.setTimeout(timer, 100);
+}
+function today() {
+	var date = new Date();
+	var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday", "Sunday"];
+	var today = days[date.getDay()];
+	var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	var todayDate = date.getDate();
+	var todayMonth = months[date.getMonth()];
+	var todayYear = date.getFullYear();
+	var dayString = today + ", " + todayMonth + " " + todayDate + " " + todayYear;
+	timeElement.innerHTML = dayString;
+}
+//disable scrolling
+document.addEventListener('touchmove', function(e) {
+	e.preventDefault();
+}, false);
